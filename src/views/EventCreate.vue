52d6a909e1a622252d66546b032591596bd300eb
shldhee <shldhee@yahoo.co.kr>
2019-04-24 00:01:11 +0900

    Completed Getters

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
-    <h1>Create Event, {{ userName }}</h1>
-    <h1>Create an Event, {{ userName }}</h1>
-    <p>This event is created by {{ userName }}</p>
+    <h1>Create Event, {{ user.name }}</h1>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event is created by {{ user.name }}</p>
+    <p>This event is created by {{ categories }}</p>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
   computed: {
-    userName() {
-      return this.$store.state.user.name
-    }
+    localComputed() {
+      return 'aa'
+    },
+    ...mapState(['user', 'categories'])
   }
 }
 </script>
